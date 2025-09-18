d40204d54a11ddd8338f486a6c7a199f95ed74e9
kanikamaheshwari04 <kanikamaheshwari210@gmail.com>
2025-09-19 00:15:50 +0530

    prime

---
+++ ifelse.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main (){
+int radius ;
+cout << "enter radius : ";
+cin >> radius;
+cout << "area : "<< (3.14*radius*radius);
+}
---
+++ prime.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int n;
+    int i;
+    cout << "enter n : ";
+    cin >>n;
+    for ( i=2;i<n;i++){
+        if (n%i==0){
+            break;
+        }
+    }
+    if (n==i){
+        cout << "prime";
+    }
+    else {
+        cout << "not";
+    }
+
+}
